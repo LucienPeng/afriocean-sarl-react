2bfa11bad8481443f4bf132e9e444f89c6853be7
Lucien Peng <kesiang.peng@gmail.com>
2022-03-27 23:38:45 +0800

    fix:path

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import HomePage from "./pages/home-page";
 import AboutPage from "./pages/about-page";
 import ProductPage from "./pages/product-page";
 import FooterComponent from "./components/footer-component";
+const url = "https://lucienpeng.github.io/afriocean-sarl-react";
 
 const App = () => {
   return (
     <div className="App">
       <NavComponent />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/product" element={<ProductPage />} />
+        <Route path={url} element={<HomePage />} />
+        <Route path={url + "/about"} element={<AboutPage />} />
+        <Route path={url + "/product"} element={<ProductPage />} />
       </Routes>
       <FooterComponent />
     </div>
