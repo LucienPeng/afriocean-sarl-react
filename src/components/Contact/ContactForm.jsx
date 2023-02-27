ed0fd644801369dfe712f397c3ab30c2e59849eb
Lucien Peng <kesiang.peng@gmail.com>
2023-02-27 21:32:46 +0800

    feat: feedback form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,104 +1,169 @@
 
 
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { Typography, Stack, Grid, FormControlLabel, Alert, Collapse } from "@mui/material";
 import { StyledTextfield, StyledCheckedBox, StyledButton } from '../UI/StyledComponents';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useToggle } from '../../utils/useToggle';
+import { useForm, Controller } from "react-hook-form";
 import jsonp from 'jsonp';
+import * as yup from "yup";
 
 const initFeedback = {
-    'entry.781707531': '',
-    'entry.1024643799': ''
+    '1074437902': '',
+    '88869021': '',
+    '166946023': '',
+    '2113753156': '',
+    '1917497005': '',
+    isAgree: false
 };
 
-const baseUrl = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSc1ZlPVPl1xe9nidLKDDpzJ5AbhDGqr_W7BXzoAVbeTC2e4Cg/formResponse`;
+const schema = yup.object({
+    '1074437902': yup.string().required(),
+    '88869021': yup.string().required(),
+    '166946023': yup.string().email().required(),
+    '2113753156': yup.number().required(),
+    '1917497005': yup.string().required(),
+    isAgree: yup.boolean().required(),
+}).required();
+
+const baseUrl = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSedP6Jw7b-KhqLZS6myoq5nSEPAyeHOYY6gPsMW5exHsr3a3w/formResponse`;
 
 export const ContactForm = () => {
-    const [feedbackForm, setFeedbackForm] = useState(initFeedback);
     const { isToggle, setIsToggle } = useToggle();
-
+    const { handleSubmit, control, reset, formState: { isDirty },
+    } = useForm({
+        resolver: yupResolver(schema),
+        defaultValues: initFeedback
+    });
     const togglerHandler = () => setIsToggle(false);
-    const unchangeHandler = (e, key) => setFeedbackForm({ ...feedbackForm, [key]: e.target.value });
 
-    const submitForm = (e) => {
-        const firstName = `entry.781707531=${feedbackForm['entry.781707531']}`;
-        const secondName = `entry.1024643799=${feedbackForm['entry.1024643799']}`;
-        const requestUrl = baseUrl + `?${firstName}&${secondName}`;
-        jsonp(requestUrl);
-        setIsToggle(true);
-        setFeedbackForm(initFeedback);
-        e.preventDefault();
+    const submitForm = (data) => {
+        if (data.isAgree) {
+            const firstName = `entry.1074437902=${data['1074437902']}`;
+            const lastName = `entry.88869021=${data['88869021']}`;
+            const email = `entry.166946023=${data['166946023']}`;
+            const phone = `entry.2113753156=${data['2113753156']}`;
+            const comment = `entry.1917497005=${data['1917497005']}`;
+            const requestUrl = baseUrl + `?${firstName}&${lastName}&${email}&${phone}&${comment}`;
+            jsonp(requestUrl);
+            setIsToggle(true);
+            reset(initFeedback);
+        } else {
+            alert('Agree');
+        }
     };
 
+    useEffect(() => {
+        if (isDirty) {
+            setIsToggle(false);
+        }
+    }, [isDirty, setIsToggle]);
+
     return (
-        <Stack component='form' maxWidth='80%' spacing={3}>
+        <Stack component='form' maxWidth='80%' spacing={3} onSubmit={handleSubmit(submitForm)}>
             <Typography variant="h1" color="common.white" fontWeight={700} fontSize={24} textAlign='center'>
                 Feedback
             </Typography>
             <Stack spacing={0.5} height='100%'>
                 <Grid container spacing={1} >
                     <Grid xs={12} sm={6} item>
-                        <StyledTextfield
-                            placeholder="Enter first name"
-                            label="First Name"
-                            variant="outlined"
-                            fullWidth
-                            onChange={(e) => unchangeHandler(e, 'entry.781707531')}
-                            value={feedbackForm['entry.781707531']}
-                            name="entry.781707531"
-                            required
+                        <Controller
+                            name="1074437902"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) =>
+                                <StyledTextfield {...field}
+                                    placeholder="Enter first name"
+                                    label="First Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    required
+                                />}
                         />
                     </Grid>
                     <Grid xs={12} sm={6} item>
-                        <StyledTextfield
-                            placeholder="Enter last name"
-                            label="Last Name"
-                            variant="outlined"
-                            onChange={(e) => unchangeHandler(e, 'entry.1024643799')}
-                            value={feedbackForm['entry.1024643799']}
-                            name="entry.1024643799"
-                            fullWidth
-                            required
+                        <Controller
+                            name="88869021"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) =>
+                                <StyledTextfield {...field}
+                                    placeholder="Enter last name"
+                                    label="Last Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    required
+                                />}
                         />
                     </Grid>
                 </Grid>
                 <Grid item xs={12}>
-                    <StyledTextfield
-                        type="email"
-                        placeholder="Enter email"
-                        label="Email"
-                        variant="outlined"
-                        fullWidth
-                    // required
+                    <Controller
+                        name="166946023"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) =>
+                            <StyledTextfield {...field}
+                                type="email"
+                                placeholder="Enter email"
+                                label="Email"
+                                variant="outlined"
+                                fullWidth
+                                required
+                            />}
                     />
                 </Grid>
                 <Grid item xs={12}>
-                    <StyledTextfield
-                        type="number"
-                        placeholder="Enter phone number"
-                        label="Phone"
-                        variant="outlined"
-                        fullWidth
-                    // required
+                    <Controller
+                        name="2113753156"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) =>
+                            <StyledTextfield {...field}
+                                type="number"
+                                placeholder="Enter phone number"
+                                label="Phone"
+                                variant="outlined"
+                                fullWidth
+                                required
+                            />}
                     />
                 </Grid>
                 <Grid item xs={12}>
-                    <StyledTextfield
-                        label="Enter your comment"
-                        multiline
-                        rows={4}
-                        placeholder="Enter your comment"
-                        variant="outlined"
-                        fullWidth
+                    <Controller
+                        name="1917497005"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) =>
+                            <StyledTextfield {...field}
+                                label="Enter your comment"
+                                multiline
+                                rows={4}
+                                placeholder="Enter your comment"
+                                variant="outlined"
+                                fullWidth
+                                required
+                            />
+                        }
                     />
                 </Grid>
             </Stack>
             <Stack spacing={3} direction='column' justifyContent='center' alignItems='center'>
-                <FormControlLabel
-                    control={<StyledCheckedBox />}
-                    label={<Typography sx={{ color: 'white' }}>I agree my submitted data is collected and stored</Typography>}
+                <Controller
+                    name="isAgree"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) =>
+                        <FormControlLabel
+                            {...field}
+                            control={<StyledCheckedBox value={field.value.isAgree} />}
+                            label={<Typography sx={{ color: 'white' }}>I agree my submitted data is collected and stored</Typography>}
+                        />
+                    }
                 />
-                <StyledButton type='submit' onClick={submitForm} alignself='center' variant="contained" color="secondary">Submit</StyledButton>
+
+                <StyledButton type='submit' alignself='center' variant="contained" color="secondary">Submit</StyledButton>
             </Stack>
             <Collapse in={isToggle}>
                 <Alert severity="success" onClose={togglerHandler}>
