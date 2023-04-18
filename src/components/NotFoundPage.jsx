ecc5e87bd2aabcbc546ffdde852cbdee0a9af39c
Lucien Peng <kesiang.peng@gmail.com>
2023-04-18 18:09:38 +0800

    feat: 404 page logo

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -3,9 +3,11 @@ import { useNavigation } from "../utils/useNavigation";
 import { StyledButton } from "./UI/StyledComponents";
 import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useDeviceMetadata } from "../utils/useDeviceMetadata";
 
 const NotFoundComponent = () => {
     const { navigationHandler } = useNavigation();
+    const { isMobileView } = useDeviceMetadata();
     const { t } = useTranslation();
     const redirectHandler = useCallback(() => navigationHandler('/'), [navigationHandler]);
     const [countdown, setCountdown] = useState(5);
@@ -15,7 +17,7 @@ const NotFoundComponent = () => {
             setCountdown(countdown - 1);
         }, 1000);
 
-        if (countdown === -1) {
+        if (countdown === 0) {
             redirectHandler();
             clearTimeout(timer);
         }
@@ -23,14 +25,23 @@ const NotFoundComponent = () => {
     }, [countdown, redirectHandler]);
 
     return (
-        <Stack minHeight='100vh' justifyContent='center' alignItems='center'>
+        <Stack bgcolor='primary.main' minHeight='100vh' justifyContent='center' alignItems='center' spacing={5}>
+            {isMobileView &&
+                <Box
+                    component='img'
+                    src='https://i.imgur.com/1UpdxEV.png'
+                    alt='logo'
+                    height={100}
+                    onClick={() => redirectHandler('/')}
+                    sx={{ cursor: 'pointer', flexGrow: 0, display: { xs: 'none', sm: 'block' } }}
+                />}
             <Box textAlign='center' mb={5}>
-                <Typography mb={3} variant="h2" fontSize='40px' fontWeight={700}>{t('404.title')}</Typography>
-                <Typography variant="h6">{t('404.paragraph1')}</Typography>
-                <Typography variant="h6">{t('404.paragraph2')}</Typography>
+                <Typography color='#fff' mb={3} variant="h2" fontSize='40px' fontWeight={700}>{t('404.title')}</Typography>
+                <Typography color='#fff' variant="h6">{t('404.paragraph1')}</Typography>
+                <Typography color='#fff' variant="h6">{t('404.paragraph2')}</Typography>
                 <Stack direction='row' spacing={2} justifyContent='center' alignItems='center'>
-                    <Typography variant="h6">{countdown}</Typography>
-                    <Typography variant="h6">{t('404.paragraph3')}</Typography>
+                    <Typography color='#fff' variant="h6">{countdown}</Typography>
+                    <Typography color='#fff' variant="h6">{t('404.paragraph3')}</Typography>
                 </Stack>
             </Box>
             <StyledButton onClick={redirectHandler} variant="contained" color="secondary">{t('404.redirect')}</StyledButton>
